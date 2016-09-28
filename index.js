16e2b8a39eaa6d891523677e8b2ef1bb8ed8b0c4
Nikolay Kostyurin <jilizart@gmail.com>
2016-09-28 15:13:17 +0200

    travis, readme, promise support

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
-const timeout = (time, ...values) => new Promise((resolve) => setTimeout(() => resolve(...values), time));
-const callback = (el, ...values) => new Promise((resolve) => resolve(el(...values)));
 const isNum = (el) => typeof el === 'number';
-const execute = (curr) => (...args) => isNum(curr) ? timeout(curr, ...args) : callback(curr, ...args);
+const isFunc = (el) => typeof el === 'function';
+const isPromise = (el) => !!el && (typeof el === 'object' || isFunc(el)) && isFunc(el.then);
+const isDefined = (el) => typeof el !== 'undefined';
 
+/**
+ * Wraps time into timeout promise
+ * @param {Number} time
+ * @param {*} value
+ * @returns {Promise}
+ */
+const timeout = (time, value) => {
+    console.log('timeout', time);
+    return new Promise(
+        (resolve) => {
+            setTimeout(() => resolve(value), time)
+        }
+    );
+};
+
+/**
+ * Wraps callback function into promise
+ * @param {Function} func
+ * @param {*} prevResult
+ * @param {Array} results
+ * @param {Function} pushResult
+ * @returns {Promise}
+ */
+const callback = (func, prevResult, results, pushResult) => {
+    const result = func(prevResult, results);
+    pushResult(result);
+
+    return new Promise(
+        (resolve) => {
+            resolve(result);
+        }
+    );
+};
+
+const promise = (el, pushResult) => {
+    return el.then((result) => {
+        pushResult(result);
+    });
+};
+
+/**
+ *
+ * @param curr
+ * @param results
+ * @returns {Promise}
+ * @throws Error
+ */
+const execute = (curr, results) => (prevResult) => {
+    const pushResult = (result) => {
+        if (isDefined(result)) results.push(result);
+    };
+
+    if (isNum(curr)) return timeout(curr, prevResult);
+    if (isPromise(curr)) return promise(curr, pushResult);
+    if (isFunc(curr)) return callback(curr, prevResult, results, pushResult);
+
+    throw new Error('Invalid value given in sequence, expected number, promise or function');
+};
+
+/**
+ * Executes array of values as promise sequence
+ * @param {Array} values
+ * @returns {Promise}
+ * @throws Error
+ */
 module.exports = (values) => new Promise(
-    (resolve, reject) => values
-        .reduce(
-            (prev, curr) => prev.then(execute(curr), reject),
-            Promise.resolve(false)
-        )
-        .then(resolve, reject)
+    (resolve, reject) => {
+        const results = [];
+
+        if (!Array.isArray(values)) {
+            throw new Error('promise-sequence expects array as first argument');
+        }
+
+        values
+            .reduce(
+                (prev, curr) => prev.then(execute(curr, results), reject),
+                Promise.resolve()
+            )
+            .then((...args) => resolve(results), () => reject(results))
+    }
 );
--- test/index.js
+++ test/index.js
@@ -5,58 +5,138 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 const seq = require('../index.js');
+const timeout = (time) => new Promise((resolve) => setTimeout(() => resolve(), time))
 const values = [
-    (...args) => {
-        console.log(...args);
+    () => {
+        return {moveCircleToMiddle: true};
+    },
+    () => {
+        return {showGrayCircle: true};
+    },
+    () => {
+        return {showMicrophone: true};
+    },
+    () => {
+        return {moveCircleToTop: true};
+    },
+    () => {
+        return {pulseGrayCircle: true};
+    },
+    () => {
+        return {okText: 1};
+    },
+    () => {
+        return {okText: 2};
+    },
+    () => {
+        return {googleText: 1};
+    },
+    () => {
+        return {googleText: 2};
+    }
+];
+const valuesWithDelays = [
+    () => {
         return {moveCircleToMiddle: true};
     },
     100,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {showGrayCircle: true};
     },
-    (...args) => {
-        console.log(...args);
+    () => {
         return {showMicrophone: true};
     },
     500,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {moveCircleToTop: true};
     },
     100,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {pulseGrayCircle: true};
     },
     500,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {okText: 1};
     },
     500,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {okText: 2};
     },
     500,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {googleText: 1};
     },
     500,
-    (...args) => {
-        console.log(...args);
+    () => {
         return {googleText: 2};
     }
 ];
 
-describe('promise sequence', () => {
-    it('should execute sequence', (done) => {
-        seq(values).then((...args) => {
-            console.log(args);
+const valuesWithPromises = [
+    () => {
+        return {moveCircleToMiddle: true};
+    },
+    timeout(100),
+    () => {
+        return {showGrayCircle: true};
+    },
+    () => {
+        return {showMicrophone: true};
+    },
+    timeout(500),
+    () => {
+        return {moveCircleToTop: true};
+    },
+    timeout(100),
+    () => {
+        return {pulseGrayCircle: true};
+    },
+    timeout(500),
+    () => {
+        return {okText: 1};
+    },
+    timeout(500),
+    () => {
+        return {okText: 2};
+    },
+    timeout(500),
+    () => {
+        return {googleText: 1};
+    },
+    timeout(500),
+    () => {
+        return {googleText: 2};
+    }
+];
 
-            done();
-        })
+const result = [
+    {moveCircleToMiddle: true},
+    {showGrayCircle: true},
+    {showMicrophone: true},
+    {moveCircleToTop: true},
+    {pulseGrayCircle: true},
+    {okText: 1},
+    {okText: 2},
+    {googleText: 1},
+    {googleText: 2}
+];
+
+describe('promise sequence', () => {
+    it('should execute sequence of functions', () => {
+        return seq(values).then((res) => {
+            expect(res).to.be.eql(result);
+        });
+    });
+
+    it('should execute sequence of functions with delay', () => {
+        return seq(valuesWithDelays).then((res) => {
+            expect(res).to.be.eql(result);
+        });
+    });
+
+    it('should execute sequence of functions with promises', () => {
+        return seq(valuesWithPromises).then((res) => {
+            expect(res).to.be.eql(result);
+        });
     });
 });
